584e8aae7e327fba44b7a82130230d931f57d897
wan <hubhy@nate.com>
2023-07-11 20:49:01 +0900

    두번째 커밋

--- src/main/java/model/player/Player.java
+++ src/main/java/model/player/Player.java
@@ -1,5 +1,27 @@
 package model.player;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.sql.Timestamp;
+
+
+@Getter
 public class Player {
     // 모델링 자리 입니다 : )
+
+    private Integer id;
+    private Integer teamId;
+    private String name;
+    private String position;
+    private Timestamp createdAt;
+
+    @Builder
+    public Player(Integer id, Integer teamId, String name, String position, Timestamp createdAt) {
+        this.id = id;
+        this.teamId = teamId;
+        this.name = name;
+        this.position = position;
+        this.createdAt = createdAt;
+    }
 }
