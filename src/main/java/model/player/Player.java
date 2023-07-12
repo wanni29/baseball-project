b5c1ff247d58ef01cbc1ac856a5654f8bdb13db6
unjoo31 <unjoo31@gmail.com>
2023-07-12 09:08:09 +0900

    20230712

--- src/main/java/model/player/Player.java
+++ src/main/java/model/player/Player.java
@@ -1,15 +1,21 @@
 package model.player;
+
 import lombok.Builder;
 import lombok.Getter;
+
 import java.sql.Timestamp;
+
+
 @Getter
 public class Player {
     // 모델링 자리 입니다 : )
+
     private Integer id;
     private Integer teamId;
     private String name;
     private String position;
     private Timestamp createdAt;
+
     @Builder
     public Player(Integer id, Integer teamId, String name, String position, Timestamp createdAt) {
         this.id = id;
